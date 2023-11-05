a63625c01fbb63456182554798baeaa57ea72981
solomonhatendi <solomonc19@gmail.com>
2023-11-05 16:52:34 -0500

    Added javascript

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,20 @@
-/* Add your JavaScript to this file */
+/* Add your JavaScript to this file*/
+window.onload = function() {
+    /*let button = document.querySelector("form button.btn");*/
+    let mailForm = document.querySelector("section.newsletter form");
+
+    let submitMessage = document.querySelector("section.newsletter .container .message");
+
+    function checkEmail(){
+        return true;
+    }
+
+    mailForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        if(mailForm["email"].value == "" || checkEmail() == false) {
+            submitMessage.innerHTML = "Please enter a valid email address";
+        } else {
+            submitMessage.innerHTML = "Thank you! Your email address has been added to our mailing list!";
+        }
+    });
+}
